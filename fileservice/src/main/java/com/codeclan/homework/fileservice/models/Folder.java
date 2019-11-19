316e476d82dbc2d3def880d8dad5d329ba7dec7c
Rebecca Still <beckystill@hotmail.com>
2019-11-19 15:51:28 +0000

    add Folder class

--- fileservice/src/main/java/com/codeclan/homework/fileservice/models/Folder.java
+++ fileservice/src/main/java/com/codeclan/homework/fileservice/models/Folder.java
@@ -1,4 +1,39 @@
 package com.codeclan.homework.fileservice.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+    private String title;
+    private List<File> files;
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.files = new ArrayList<File>();
+        this.user = user;
+    }
+
+    public Folder() {
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void addFile(File file) {
+        this.files.add(file);
+    }
 }
